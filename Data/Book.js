63e143440137b56ab38a861a4f5fe2f2cc7b7802
zeycoder <zynparsln.5561@gmail.com>
2023-06-04 20:39:43 +0300

    code edited

--- Data/Book.js
+++ Data/Book.js
@@ -1,107 +1,107 @@
 export const Book = [
-    {
-        "BookID": 1,
-        "BookName": "Kırmızı Pazartesi",
-        "BookImage": "image123",
-        "Author": "Gabriel Garcia Marquez",
-        "Follower":[]
-    },
-    {
-        "BookID": 2,
-        "BookName": "Beyaz Geceler",
-        "BookImage": "image123",
-        "Author": "Fyodor Dostoyevski",
-        "Follower":[]
-    },
-    {
-        "BookID": 3,
-        "BookName": "1984",
-        "BookImage": "image123",
-        "Author": "George Orwell",
-        "Follower":[]
-    },
-    {
-        "BookID": 4,
-        "BookName": "Bir Kadının Yaşamından 24 Saat",
-        "BookImage": "image123",
-        "Author": "Stefan Zweig",
-        "Follower":[]
-    },
-    {
-        "BookID": 5,
-        "BookName": "Yeraltından Notlar",
-        "BookImage": "image123",
-        "Author": "Fyodor Dostoyevski",
-        "Follower":[]
-    },
-    {
-        "BookID": 6,
-        "BookName": "Sefiller",
-        "BookImage": "image123",
-        "Author": "Victor Hugo",
-        "Follower":[]
-    },
-    {
-        "BookID": 7,
-        "BookName": "Simyacı",
-        "BookImage": "image123",
-        "Author": "Paulo Coelho",
-        "Follower":[]
-    },
-    {
-        "BookID": 8,
-        "BookName": "Anna Karenina",
-        "BookImage": "image123",
-        "Author": "Leo Tolstoy",
-        "Follower":[]
-    },
-    {
-        "BookID": 9,
-        "BookName": "Harry Potter ve Felsefe Taşı",
-        "BookImage": "image123",
-        "Author": "J.K. Rowling",
-        "Follower":[]
-    },
-    {
-        "BookID": 10,
-        "BookName": "Yüzüklerin Efendisi: Yüzük Kardeşliği",
-        "BookImage": "image123",
-        "Author": "J.R.R. Tolkien",
-        "Follower":[]
-    },
-    {
-        "BookID": 11,
-        "BookName": "Dune",
-        "BookImage": "image123",
-        "Author": "Frank Herbert",
-        "Follower":[]
-    },
-    {
-        "BookID": 12,
-        "BookName": "Otomatik Portakal",
-        "BookImage": "image123",
-        "Author": "Anthony Burgess",
-        "Follower":[]
-    },
-    {
-        "BookID": 13,
-        "BookName": "Kurtlarla Dans",
-        "BookImage": "image123",
-        "Author": "Jean M. Auel",
-        "Follower":[]
-    },
-    {
-        "BookID": 14,
-        "BookName": "Savaş ve Barış",
-        "BookImage": "image123",
-        "Author": "Leo Tolstoy",
-        "Follower":[]
-    },
-    {
-        "BookID": 15,
-        "BookName": "Kör Baykuş",
-        "BookImage": "image123",
-        "Author": "Sadık Hidayet",
-        "Follower":[]
-    },
-]
+  {
+    BookID: 1,
+    BookName: "Kırmızı Pazartesi",
+    BookImage: "image123",
+    Author: "Gabriel Garcia Marquez",
+    Follower: [],
+  },
+  {
+    BookID: 2,
+    BookName: "Beyaz Geceler",
+    BookImage: "image123",
+    Author: "Fyodor Dostoyevski",
+    Follower: [],
+  },
+  {
+    BookID: 3,
+    BookName: "1984",
+    BookImage: "image123",
+    Author: "George Orwell",
+    Follower: [],
+  },
+  {
+    BookID: 4,
+    BookName: "Bir Kadının Yaşamından 24 Saat",
+    BookImage: "image123",
+    Author: "Stefan Zweig",
+    Follower: [],
+  },
+  {
+    BookID: 5,
+    BookName: "Yeraltından Notlar",
+    BookImage: "image123",
+    Author: "Fyodor Dostoyevski",
+    Follower: [],
+  },
+  {
+    BookID: 6,
+    BookName: "Sefiller",
+    BookImage: "image123",
+    Author: "Victor Hugo",
+    Follower: [],
+  },
+  {
+    BookID: 7,
+    BookName: "Simyacı",
+    BookImage: "image123",
+    Author: "Paulo Coelho",
+    Follower: [],
+  },
+  {
+    BookID: 8,
+    BookName: "Anna Karenina",
+    BookImage: "image123",
+    Author: "Leo Tolstoy",
+    Follower: [],
+  },
+  {
+    BookID: 9,
+    BookName: "Harry Potter ve Felsefe Taşı",
+    BookImage: "image123",
+    Author: "J.K. Rowling",
+    Follower: [],
+  },
+  {
+    BookID: 10,
+    BookName: "Yüzüklerin Efendisi: Yüzük Kardeşliği",
+    BookImage: "image123",
+    Author: "J.R.R. Tolkien",
+    Follower: [],
+  },
+  {
+    BookID: 11,
+    BookName: "Dune",
+    BookImage: "image123",
+    Author: "Frank Herbert",
+    Follower: [],
+  },
+  {
+    BookID: 12,
+    BookName: "Otomatik Portakal",
+    BookImage: "image123",
+    Author: "Anthony Burgess",
+    Follower: [],
+  },
+  {
+    BookID: 13,
+    BookName: "Kurtlarla Dans",
+    BookImage: "image123",
+    Author: "Jean M. Auel",
+    Follower: [],
+  },
+  {
+    BookID: 14,
+    BookName: "Savaş ve Barış",
+    BookImage: "image123",
+    Author: "Leo Tolstoy",
+    Follower: [],
+  },
+  {
+    BookID: 15,
+    BookName: "Kör Baykuş",
+    BookImage: "image123",
+    Author: "Sadık Hidayet",
+    Follower: [],
+  },
+];
